33ac2fb5b2f1f0c88e2b697f01d819f6e0709cf8
Thomas Schank <DrTom@schank.ch>
2022-03-20 09:41:58 +0100

    Init FE

--- cljc-src/cider_ci/server/main.cljs
+++ cljc-src/cider_ci/server/main.cljs
@@ -1,12 +1,11 @@
 (ns cider-ci.server.main
   (:require
     [cider-ci.shared.logging :as logging]
-    [cider-ci.server.routing]
+    [cider-ci.server.routing :as routing]
     [taoensso.timbre :refer [debug info warn error]]
     ))
 
-
 (defn ^:dev/after-load init [& args]
   (js/console.log "(re-)initializing application ...")
-
-  )
+  (logging/init)
+  (routing/init))

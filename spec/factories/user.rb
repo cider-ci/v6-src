89214782996a54464b37549b8ac13c33a653b880
Thomas Schank <DrTom@schank.ch>
2022-04-07 19:24:28 +0200

    Extend user factories

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,22 +2,37 @@ class User < Sequel::Model
   attr_accessor :password
   attr_accessor :firstname
   attr_accessor :lastname
+  attr_accessor :session_token
+  attr_accessor :email
 end
 
 FactoryBot.define do
   factory :user do
-    firstname { Faker::Name.first_name }
+    firstname { Faker::Name.unique.first_name }
     lastname { Faker::Name.unique.last_name }
     name { firstname + ' ' + lastname }
-    email { firstname + '.' + lastname + '@' + Faker::Internet.domain_name }
+    login { firstname }
+    email  {firstname + '.' + lastname + '@' + Faker::Internet.domain_name }
     password { "secret" }
+    session_token { SecureRandom.uuid }
 
     after :create do |user|
+
       password_hash = database["SELECT crypt(#{database.literal(user.password)}, " \
                          "gen_salt('bf')) AS pw_hash"].first[:pw_hash]
       database[:passwords].insert(
         user_id: user.id,
         password_hash: password_hash)
+
+      token_digest = database[
+        "SELECT encode(digest(#{database.literal(user.session_token)},'sha256'), 'base64')" \
+        "AS token_digest" ].first[:token_digest]
+      database[:sessions].insert(
+        user_id: user.id, token_digest: token_digest)
+
+      database[:email_addresses].insert(
+        user_id: user.id, email: user.email, is_primary: true)
+
     end
 
     factory :admin do

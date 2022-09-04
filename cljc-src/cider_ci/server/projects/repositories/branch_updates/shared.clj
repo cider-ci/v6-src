c9b2ccc1398e92e21e158b58838d5b86afdc38ae
Thomas Schank <DrTom@schank.ch>
2022-09-04 16:08:02 +0200

    WIP ...

---
+++ cljc-src/cider_ci/server/projects/repositories/branch_updates/shared.clj
@@ -0,0 +1,29 @@
+; Copyright © 2013 - 2016 Dr. Thomas Schank <Thomas.Schank@AlgoCon.ch>
+; Licensed under the terms of the GNU Affero General Public License v3.
+; See the "LICENSE.txt" file provided with this software.
+
+(ns cider-ci.server.projects.repositories.branch-updates.shared
+  (:refer-clojure :exclude [str keyword])
+  (:require [cider-ci.utils.core :refer [keyword str]])
+  (:require
+    [cider-ci.server.projects.repositories.branch-updates.db-schema :as db-schema]
+    [cider-ci.server.projects.repositories.state.main :as state]
+    [tick.core :refer [now]]
+    [schema.core :as schema]))
+
+
+(defn db-update-branch-updates [id fun]
+  (state/update-in-repository
+    id (fn [repository]
+         (let [updated-repo
+               (-> repository
+                   (update-in [:branch-updates] fun)
+                   (update-in [:branch-updates] #(assoc % :updated_at (now))))]
+           (schema/validate db-schema/schema (:branch-updates updated-repo))
+           updated-repo))))
+
+(defn db-get-branch-updates [id]
+  (-> (state/get-db) :repositories (get (keyword id)) :branch-updates))
+
+;### Debug ####################################################################
+;(debug/debug-ns *ns*)
--- cljc-src/cider_ci/server/projects/repositories/fetch_and_update/db_schema.clj
+++ cljc-src/cider_ci/server/projects/repositories/fetch_and_update/db_schema.clj
@@ -1,4 +1,4 @@
-; Copyright © 2013 - 2016 Dr. Thomas Schank <Thomas.Schank@AlgoCon.ch>
+; Copyright © 2013 - 2022 Dr. Thomas Schank <Thomas.Schank@AlgoCon.ch>
 ; Licensed under the terms of the GNU Affero General Public License v3.
 ; See the "LICENSE.txt" file provided with this software.
 
@@ -7,25 +7,22 @@
   (:require
     [cider-ci.utils.core :refer [keyword str]]
     [schema.core :as schema]
-    [tick.core :as tick]
+    [tick.core :as tick :refer [now]]
     )
-  (:import [java.time Instant])
-
-  )
+  (:import [java.time Instant]))
 
 (defn default []
   {
    :last_fetched_at nil
    :last_error nil
    :last_error_at nil
-   :updated_at (tick/now)
+   :updated_at (now)
    :pending? true
    :state "initializing"
    })
 
 (def schema
-  {
-   :last_fetched_at (schema/maybe Instant)
+  {:last_fetched_at (schema/maybe Instant)
    :last_error (schema/maybe String)
    :last_error_at (schema/maybe Instant)
    :updated_at Instant
@@ -34,9 +31,7 @@
             "fetching"
             "initializing"
             "ok"
-            "waiting"
-            )
-   :pending? Boolean
-   })
+            "waiting")
+   :pending? Boolean })
 
 (schema/validate schema (default))
